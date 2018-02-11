3a4798aaaed50d31d5d873d8e13f4425178d0906
Ali <alikiyand@gmail.com>
2018-02-11 19:01:14 +0000

    adding responsive side menu for admin

--- admin-panel.php
+++ admin-panel.php
@@ -5,10 +5,25 @@ require_once "nav.phtml"
 
 <body id="admin-page">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="./assets/css/a.css">
-<link rel="stylesheet" href="./assets/css/b.css">
+<link rel="stylesheet" href="./assets/css/adminStyle.css">
 
-<div id="wrapper">
+<div class="container-fluid">
+  <div id="sideNav" class="col-xs-3 col-md-2 animated fadeInLeft">
+    <ul class="sideLinks">
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+      <li><a href="Users.php" class="btn btn-success"><i class="fa fa-user-circle" aria-hidden="true"></i>  Users</a></li>
+    </ul>
+</div>
+
+
+
 
 
 </div>
