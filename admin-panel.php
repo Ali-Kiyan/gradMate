39c8444961295f43141245137088c0963e9c83b5
Ali <alikiyand@gmail.com>
2018-03-08 00:35:22 +0000

    added hamburger

--- admin-panel.php
+++ admin-panel.php
@@ -5,6 +5,7 @@ $connect = mysqli_connect("localhost","root","root","jobWizard");
 $query = "Select * FROM company ORDER BY company_id DESC LIMIT 12";
 $result = mysqli_query($connect, $query);
 ?>
+<script src="./assets/js/script.js"></script>
 
 <body id="admin-page">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -20,7 +21,7 @@ $result = mysqli_query($connect, $query);
   require_once "adminSideNav.phtml";
   ?>
 
-  <div class=" animated fadeInUp scrollable" id="tier2Table">
+  <div class="col-xs-12 animated fadeInUp scrollable" id="tier2Table">
    <table id="companyData" class="table table-hover table-bordered table-responsive ">
    <thead>
      <tr>
@@ -74,5 +75,65 @@ require_once "footer.phtml";
  .table-hover > tbody > tr:hover {
    background-color: #f6f7db;
  }
+
+ .hamburger{
+   display: inline-block;
+   margin-left: 42vw;
+ }
+ .hamMargin{
+      margin-left: 50vw;
+
+ }
+
+ .hamburger .line{
+  width: 50px;
+  height: 5px;
+  background-color: #ecf0f1;
+  display: block;
+  margin: 8px auto;
+  -webkit-transition: all 0.3s ease-in-out;
+  -o-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+
+}
+
+
+ #hamburger-6.is-active{
+  -webkit-transition: all 0.3s ease-in-out;
+  -o-transition: all 0.3s ease-in-out;
+  transition: all 0.3s ease-in-out;
+  -webkit-transition-delay: 0.6s;
+  -o-transition-delay: 0.6s;
+  transition-delay: 0.6s;
+  -webkit-transform: rotate(45deg);
+  -ms-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+}
+
+#hamburger-6.is-active .line:nth-child(2){
+  width: 0px;
+}
+
+#hamburger-6.is-active .line:nth-child(1),
+#hamburger-6.is-active .line:nth-child(3){
+  -webkit-transition-delay: 0.3s;
+  -o-transition-delay: 0.3s;
+  transition-delay: 0.3s;
+}
+
+#hamburger-6.is-active .line:nth-child(1){
+  -webkit-transform: translateY(13px);
+  -ms-transform: translateY(13px);
+  -o-transform: translateY(13px);
+  transform: translateY(13px);
+}
+
+#hamburger-6.is-active .line:nth-child(3){
+  -webkit-transform: translateY(-13px) rotate(90deg);
+  -ms-transform: translateY(-13px) rotate(90deg);
+  -o-transform: translateY(-13px) rotate(90deg);
+  transform: translateY(-13px) rotate(90deg);
+}
 
 </style>
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,16 @@ $('#showhideAnalysis').click(function(){
 $('#showhideCV').click(function(){
   $('#cvs').slideToggle(300);
 });
-$('#sideNavCtlr').click(function(){
+$('#hamburger-6').click(function(){
   $('#sideNav').slideToggle(700);
+
   setTimeout(function () {
       $('#tier2Table').toggleClass('col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2 animated fadeIn');
-  }, 500);
+      $('.hamburger').toggleClass('hamMargin');
+  }, 400);
 
 });
+
+  $(".hamburger").click(function() {
+    $(this).toggleClass("is-active");
+  });
