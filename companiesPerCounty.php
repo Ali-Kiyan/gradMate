07483c09dbc0d367a564379ed4bd680ab2231416
Ali <alikiyand@gmail.com>
2018-03-09 00:53:38 +0000

    enhanced visualisation

--- companiesPerCounty.php
+++ companiesPerCounty.php
@@ -1,6 +1,6 @@
 <?php
 $connect = mysqli_connect("localhost","root","root","jobWizard");
-$query = "select count(company_name) as companies, county from company group by county ";
+$query = "select distinct count(company_name) as companies, town from company group by county ";
 $result = mysqli_query($connect, $query);
 $json_array = array();
 while($row = mysqli_fetch_array($result))
