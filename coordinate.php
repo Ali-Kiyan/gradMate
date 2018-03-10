317ee8d400d498294abf24e001ae1f83407056e3
Ali <alikiyand@gmail.com>
2018-03-10 19:30:12 +0000

    getting lat and long from api

--- coordinate.php
+++ coordinate.php
@@ -6,7 +6,8 @@ $city = 'Manchester';
 $apiToken = 'pk.eyJ1IjoiYWxpa2l5YW4iLCJhIjoiY2pla21xamxjMTJ3ZDMzcGhhcWVib3dxYSJ9.jZ0kXaBcdetnYe-5NiFyug';
 $apiURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' . $city . '.json?access_token=' . $apiToken . '&country=gb&limit=1';
 $city_data = file_get_contents($apiURL);
-$json = json_decode($city_data, TRUE);
-echo $city_data;
-
+// echo $city_data;
+$mapboxContent = json_decode($city_data, true);
+echo $s['features'][0]['center'][0];
+echo $s['features'][0]['center'][1];
 ?>
