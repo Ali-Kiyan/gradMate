b0e30373213192538dccd03fe0041f2c5f6b9106
Ali <alikiyand@gmail.com>
2018-03-10 19:48:06 +0000

    passing json file

--- coordinate.php
+++ coordinate.php
@@ -1,13 +1,34 @@
 <?php
 
-// https://api.mapbox.com/geocoding/v5/mapbox.places/Manchester.json?access_token=pk.eyJ1IjoiYWxpa2l5YW4iLCJhIjoiY2pla21xamxjMTJ3ZDMzcGhhcWVib3dxYSJ9.jZ0kXaBcdetnYe-5NiFyug&limit=
 
 $city = 'Manchester';
 $apiToken = 'pk.eyJ1IjoiYWxpa2l5YW4iLCJhIjoiY2pla21xamxjMTJ3ZDMzcGhhcWVib3dxYSJ9.jZ0kXaBcdetnYe-5NiFyug';
 $apiURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' . $city . '.json?access_token=' . $apiToken . '&country=gb&limit=1';
+// $connect = mysqli_connect("localhost","root","root","jobWizard");
+// $query = "select distinct count(company_name) as companies, town from company group by county ";
+// $result = mysqli_query($connect, $query);
+// $json_array = array();
+// while($row = mysqli_fetch_array($result))
+// {
+//   $json_array[] = $row;
+//
+// }
+// // var_dump($json_array);
+// $cPerCounty = json_encode($json_array);
+// echo $cPerCounty;
+
+// $cities = [a,s,s];
+// for($i=0; $i<sizeof($cities);$i++){
+//   echo "hi";
+// }
+
 $city_data = file_get_contents($apiURL);
-// echo $city_data;
 $mapboxContent = json_decode($city_data, true);
-echo $s['features'][0]['center'][0];
-echo $s['features'][0]['center'][1];
+$longitude = $mapboxContent['features'][0]['center'][0];
+$latitude =  $mapboxContent['features'][0]['center'][1];
+$cities['lat']=$latitude;
+$cities['long']=$longitude;
+$cities['city']=$city;
+$y = json_encode($cities);
+
 ?>
