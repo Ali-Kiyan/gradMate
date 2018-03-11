b78c16d03cdfa1603b06734b587741ed538f0746
Ali <alikiyand@gmail.com>
2018-03-11 22:12:11 +0000

    getting different radius circle from api

--- coordinate.php
+++ coordinate.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 $city = 'Manchester';
-$apiToken = 'pk.eyJ1IjoiYWxpa2l5YW4iLCJhIjoiY2pla21xamxjMTJ3ZDMzcGhhcWVib3dxYSJ9.jZ0kXaBcdetnYe-5NiFyug';
+$apiToken = 'pk.eyJ1IjoiYWxpa2l5YW55IiwiYSI6ImNqZW43Mm9wYzBmOW8yd3BiZHMzcm9kcG4ifQ.dOhD9h204eeqVa-dLMqRxQ';
 $apiURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' . $city . '.json?access_token=' . $apiToken . '&country=gb&limit=1';
 // $connect = mysqli_connect("localhost","root","root","jobWizard");
 // $query = "select distinct count(company_name) as companies, town from company group by county ";
@@ -25,12 +25,19 @@ $city_data = file_get_contents($apiURL);
 $mapboxContent = json_decode($city_data, true);
 $longitude = $mapboxContent['features'][0]['center'][0];
 $latitude =  $mapboxContent['features'][0]['center'][1];
+$numOfCompany = log(500)*2;
 $cities[0]['long']=$longitude;
 $cities[0]['lat']=$latitude;
 $cities[0]['city']=$city;
+$cities[0]['numOfCompany']=$numOfCompany;
 $cities[1]['long']=-2.9915726;
 $cities[1]['lat']=53.4083714 ;
 $cities[1]['city']='Liverpool';
+$cities[1]['numOfCompany']=log(200)*2;
+$cities[2]['long']=-2.1;
+$cities[2]['lat']=54.4083714 ;
+$cities[2]['city']='aa';
+$cities[2]['numOfCompany']=log(100)*2;
 $apiData = json_encode($cities);
 echo $apiData;
 ?>
