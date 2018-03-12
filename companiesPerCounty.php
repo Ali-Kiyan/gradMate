5302a2131a8c8271c244b6d677f9f2a0cfe8653e
Ali <alikiyand@gmail.com>
2018-03-12 19:56:49 +0000

    inner api for map for companies is working

--- companiesPerCounty.php
+++ companiesPerCounty.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 $connect = mysqli_connect("localhost","root","root","jobWizard");
-$query = "select distinct count(company_name) as companies, town from company group by county ";
+$query = "select distinct count(company_name) as companies, county from company where county != ' ' group by county having count(company_name)>0";
 $result = mysqli_query($connect, $query);
 $json_array = array();
 
@@ -12,7 +12,7 @@ while($row = mysqli_fetch_array($result))
   $json_array[] = $row;
 }
 
-$cPerCounty = json_encode($json_array); 
+$cPerCounty = json_encode($json_array);
 echo $cPerCounty;
 
 ?>
