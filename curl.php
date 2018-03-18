ae75cc8f10795cb719f1956ddcae37465a6aa6a3
Ali <alikiyand@gmail.com>
2018-03-18 19:24:28 +0000

    posting data with curl

--- curl.php
+++ curl.php
@@ -2,10 +2,17 @@
 
 $url = "http://localhost:8888/dissertation/coordinate.php";
 $client = curl_init($url);
+//for setting it in a seperate variable you have to enable a curl option
+
 curl_setopt($client, CURLOPT_RETURNTRANSFER,1);
+//for posting the data with curl
+// curl_setopt($client, CURLOPT_POST,$data);
+
+
 $response = curl_exec($client);
-echo $response;
-echo "hi";
+echo $client;
+$result= json_decode($response);
+var_dump($result);
 
 
 
