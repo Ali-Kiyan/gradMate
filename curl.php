362815974867c1c23ca7ad44840fe55bd77efb79
Ali <alikiyand@gmail.com>
2018-03-27 05:22:22 +0100

    adding new city to analysis

--- curl.php
+++ curl.php
@@ -1,10 +1,14 @@
 <?php
 
 $url = "http://localhost:8888/dissertation/coordinate.php";
-$client = curl_init($url);
+$client = curl_init();
+$curl_setopt($client, CURL_URL, $url);
+$curlcurl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+$curlcurl_setopt($client, CURLOPT_POST, 1);
+$curlcurl_setopt($client, CURLOPT_POST, 1);
 //for setting it in a seperate variable you have to enable a curl option
 
-curl_setopt($client, CURLOPT_RETURNTRANSFER,1);
+// curl_setopt($client, CURLOPT_RETURNTRANSFER,1);
 //for posting the data with curl
 // curl_setopt($client, CURLOPT_POST,$data);
 
