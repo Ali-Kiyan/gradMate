556b5737ec3f41406d832c8676643aaea63ea68e
Ali <alikiyand@gmail.com>
2018-04-04 21:07:30 +0100

    adding blured background for some pages in navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,11 @@ $('#showhideCV').click(function(){
   $('#cvs').slideToggle(400);
 });
 $('#hamburger-6').click(function(){
-  $('#sideNav').toggle("slide");
-      $('.hamburger').toggleClass('hamMargin',100);
+  $('#sideNav').toggle("slide",600);
+      $('.hamburger').toggleClass('hamMargin',1000);
       $('#tier2table').removeClass('col-xs-12');
       $('#tier2Table').toggleClass('col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2',200);
+      $('.container').toggleClass('blur-all');
 });
 
   $(".hamburger").click(function() {
