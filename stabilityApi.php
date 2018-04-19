67555327f0a4419aa1deead1e41e7ab042a86ff8
Ali <alikiyand@gmail.com>
2018-04-19 02:20:29 +0100

    stability analysis

---
+++ stabilityApi.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json');
+$connect = mysqli_connect("localhost","root","root","jobWizard");
+$query = "select l.longitude,l.latitude, count(company_id) as numOfCompany from company as c inner join locationDetail as l on l.location = c.county where year(now()) - year(date_added) > 3 and date_added != '0000-00-00' and county != '' and industry = '" . $_GET['industry'] . "'GROUP by county ";
+$result = mysqli_query($connect, $query);
+$json_array = array();
+
+while($row = mysqli_fetch_array($result))
+{
+  $json_array[] = $row;
+}
+for($i=0;$i<sizeof($json_array);$i++){
+  unset($json_array[$i][0]);
+  unset($json_array[$i][1]);
+  unset($json_array[$i][2]);
+}
+$result = json_encode($json_array);
+echo $result;
+
+?>
