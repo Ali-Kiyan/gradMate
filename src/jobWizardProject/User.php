060745e46c5aa7f78c239e118cc141203ccfd211
Ali <alikiyand@gmail.com>
2018-04-28 11:42:06 +0100

    adding userSchema, User class and table abstract

---
+++ src/jobWizardProject/User.php
@@ -0,0 +1,33 @@
+<?php
+namespace jobWizardProject;
+class User {
+  protected $User_id, $First_Name, $Last_Name, $username, $email, $password, $add1, $add2, $phone, $postcode, $dob, $degree;
+  public function __construct($dbrow) {
+    $this->User_id = $dbrow['User_id'];
+    $this->First_Name = $dbrow['First_Name'];
+    $this->Last_Name = $dbrow['Last_Name'];
+    $this->username = $dbrow['username'];
+    $this->email = $dbrow['email'];
+    $this->password = $dbrow['password'];
+    $this->add1 = $dbrow['add1'];
+    $this->add2 = $dbrow['add2'];
+    $this->phone = $dbrow['phone'];
+    $this->postcode = $dbrow['postcode'];
+    $this->dob = $dbrow['dob'];
+    $this->degree = $dbrow['degree'];
+}
+  //accessors
+  public function getUserId() { return $this->User_id;}
+  public function getFirstName() { return $this->First_Name;}
+  public function getLastName() { return $this->Last_Name;}
+  public function getUsername() { return $this->username;}
+  public function getEmail() { return $this->email;}
+  public function getPassword() { return $this->password;}
+  public function getAdd1() { retrun $this->add1;}
+  public function getAdd2() { retrun $this->add2;}
+  public function getPhone() {return $this->phone;}
+  public function getPostcode() {return $this->postcode;}
+  public function getDOB() {return $this->dob;}
+  public function getDegree() {return $this->degree;}
+
+}
