07410ef2af419ff9ba4907d255b3ca49ea73546e
Ali <alikiyand@gmail.com>
2018-04-29 13:03:16 +0100

    altering signup and login form to conform to login page

---
+++ register.php
@@ -0,0 +1,24 @@
+<?php
+$view = new stdClass();
+$view->pageTitle = 'Sign Up';
+require_once './vendor/autoload.php';
+
+if(isset($_POST['Rsubmit']))
+{
+    $handle = new jobWizardProject\UserTable();
+
+    $response = $handle->insertUser($_POST);
+
+     if(!$response)
+     {
+         $view->result = '<div class="alert alert-danger">Please check you input </div>';
+     }
+     else
+     {
+         $view->result = '<div class="alert alert-success">You are successfully added to the database, log in !</div>';
+     }
+
+}
+
+
+require_once  './Views/register.phtml';
