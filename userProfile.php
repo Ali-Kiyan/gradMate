e4dd67eea309a68b848ab3d3b36c713bbb2eee49
Ali <alikiyand@gmail.com>
2018-04-29 11:43:58 +0100

    users editing

---
+++ userProfile.php
@@ -0,0 +1,34 @@
+<?php
+// require_once  "./Views/template/included_functions.php";
+// confirm_logged_in ();
+$view = new stdClass();
+$view->pageTitle = 'User Profile';
+require_once  './vendor/autoload.php';
+
+$userdb = new JobWizardProject\UserTable();
+$Current_User = $userdb->fetchRecord($_SESSION['User_id']);
+
+if(isset($_POST['Usubmit']))
+{
+    $userdb = new jobWizardProject\UserTable();
+    $_SESSION["First_Name"] = $_POST["First_Name"];
+    $_SESSION["Username"] = $_POST["Username"];
+    $_SESSION["Password"] = $_POST["Password"];
+
+
+    $respond = $userdb->editUser($_POST);
+
+    if($respond)
+    {
+        redirect_to("./UserProfile.php");
+        $view->result = '<div class="alert alert-success">Successfully Updated </div>';
+    }
+    else
+    {
+        $view->result = '<div class="alert alert-danger">Please check your input </div>';
+    }
+
+}
+
+
+require_once './Views/userprofile.phtml';
