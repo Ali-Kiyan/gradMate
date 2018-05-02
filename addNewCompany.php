deea2d73586085b15b0946a936b0b551079771a9
Ali <alikiyand@gmail.com>
2018-05-02 19:26:37 +0100

    designing oop and MVC structure for CompanyHouse API

--- addNewCompany.php
+++ addNewCompany.php
@@ -1,4 +1,34 @@
 <?php
+// require('./Views/Template/includedFunctions.php');
+// confirmLoggedIn();
+session_start();
+$view = new stdClass();
+$view->pageTitle = 'Add New Company';
+require_once  './vendor/autoload.php';
+
+if(isset($_POST['Asubmit'])){
+  $API = new JobWizardProject\ApiCompany();
+  $generalInfo = $API->fetchGeneralInfo($_POST['Company_Name']);
+  $Current_Company = $API->fetchCompanyInfo($generalInfo);
+}
+
+
+if(isset($_POST['Isubmit']))
+{
+
+    $_SESSION['Company_Id'] = $_POST['Company_Id'];
+    $respond = $companydb->editCompany($_POST);
+    if($respond)
+    {
+        redirectTo("./editCompany.php");
+        $view->result = '<div class="alert alert-success">Successfully Updated </div>';
+    }
+    else
+    {
+        $view->result = '<div class="alert alert-danger">Please check your input </div>';
+    }
+
+}
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,27 @@
-$('#showhideAnalysis').click(function(){
-  $('#charts').slideToggle(400);
-});
-$('#showhideCV').click(function(){
-  $('#cvs').slideToggle(400);
-});
-$('#hamburger-6').click(function(){
-  $('#sideNav').toggle("slide",600);
-      $('.hamburger').toggleClass('hamMargin',1000);
-      $('#tier2Table').toggleClass('col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2',200);
-      $('.chart-container').toggleClass('col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2',600);
-      $('.bluredSection').toggleClass('blur-all');
-});
+$(document).ready(function(){
 
-  $(".hamburger").click(function() {
-    $(this).toggleClass("is-active",300);
+  TweenLite.fromTo($('#usersTable'),2,{y: 30}, {y:0, opacity: 1});
+
+
+
+
+
+  $('#showhideAnalysis').click(function(){
+    $('#charts').slideToggle(400);
   });
-    TweenLite.fromTo($('#large-header'),1.5,{y:10,ease: "easeOut.config(1.7)"}, {y:0,opacity:1,ease: "easeOut.config(1.7)"});
+  $('#showhideCV').click(function(){
+    $('#cvs').slideToggle(400);
+  });
+  $('#hamburger-6').click(function(){
+    $('#sideNav').toggle("slide",600);
+        $('.hamburger').toggleClass('hamMargin',1000);
+        $('#tier2Table').toggleClass('col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2',200);
+        $('.chart-container').toggleClass('col-xs-9 col-xs-offset-3 col-md-10 col-md-offset-2',600);
+        $('.bluredSection').toggleClass('blur-all');
+  });
+
+    $(".hamburger").click(function() {
+      $(this).toggleClass("is-active",300);
+    });
+      TweenLite.fromTo($('#large-header'),1.5,{y:10,ease: "easeOut.config(1.7)"}, {y:0,opacity:1,ease: "easeOut.config(1.7)"});
+});
