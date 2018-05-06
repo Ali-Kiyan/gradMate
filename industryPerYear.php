f95e65af70a96a2d3fe560e212acf6c3fc1e948d
Ali <alikiyand@gmail.com>
2018-05-06 11:27:44 +0100

    cleanig up

--- industryPerYear.php
+++ industryPerYear.php
@@ -3,8 +3,5 @@ require_once './vendor/autoload.php';
 
 $API = new jobWizardProject\InnerAPI();
 $industries = $API->AllIndustries();
-// for($i=0;$i<sizeof($indstries);$i++){
-//   echo $indstries[$i]["Industry"];
-// }
 require_once "./Views/industryPerYear.phtml";
 ?>
