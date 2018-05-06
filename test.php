1e4224e5970beba4380d95c6d5d82744efc828b9
Ali <alikiyand@gmail.com>
2018-05-06 11:34:30 +0100

    cleaning up

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file1= 'a.pdf';
-header('Content-type:application/pdf');
-header('Content-Description:inline; filename="' .$file1. '"');
-header('Content-Transfer-Encoding:binary');
-header('Accept-Ranges: bytes');
-@readfile($file1);
-echo "hi";
-?>
