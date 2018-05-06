3330b160a4eee92cd726e6425a898b72deecfd96
Ali <alikiyand@gmail.com>
2018-05-06 22:15:20 +0100

    minor improvements

--- Database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$view = new stdClass();
-$view->pageTitle = 'Database';
-require_once './vendor/autoload.php';
-$database = new jobWizardProject\UserTable();
-$view->userList = $database->fetchAllUsers();
-var_dump($view->userList);
