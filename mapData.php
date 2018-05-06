0c7307b67ee3adf823183570aed550de1e61576e
Ali <alikiyand@gmail.com>
2018-05-06 20:54:24 +0100

    directoy test for heroku

--- mapData.php
+++ mapData.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once "../Views/Template/includedFunctions.php";
+require_once "./Views/Template/includedFunctions.php";
 // confirmAdmin();
-require_once '../vendor/autoload.php';
+require_once './vendor/autoload.php';
 $companies = new jobWizardProject\InnerAPI();
 $companiesPerCounty = $companies->companiesPerCounty();
 $CompaniesPerCordinate = $companies->CompaniesPerCordinate($companiesPerCounty);
