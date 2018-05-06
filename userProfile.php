1ecb5bb1a97603153d540238ec929a28891b22ae
Ali <alikiyand@gmail.com>
2018-05-06 22:33:16 +0100

    test

--- userProfile.php
+++ userProfile.php
@@ -1,12 +1,11 @@
 <?php
 require_once "./Views/Template/includedFunctions.php";
-// confirmLoggedIn();
+confirmLoggedIn();
 $view = new stdClass();
 $view->pageTitle = 'User Profile';
 require_once  './vendor/autoload.php';
-
 $userdb = new JobWizardProject\UserTable();
-// $Current_User = $userdb->fetchUserInfo($_SESSION['User_id']);
+$Current_User = $userdb->fetchUserInfo($_SESSION['User_id']);
 // if(isset($_POST['Usubmit']))
 // {
 //     $userdb = new jobWizardProject\UserTable();
