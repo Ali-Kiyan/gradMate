eba56845b1185407edd01b761b1075d4b3e2dee3
Ali <alikiyand@gmail.com>
2018-05-06 13:14:55 +0100

    test

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,6 +1,6 @@
 <?php
-require('./Views/Template/includedFunctions.php');
-confirmAdmin();
+// require('./Views/Template/includedFunctions.php');
+// confirmAdmin();
 require('./Views/Template/header.phtml');
 require('./Views/Template/adminNav.phtml');
 require('./Views/Template/AdminSideNav.phtml');
