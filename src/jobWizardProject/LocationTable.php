da475e37cfb1cb647ea830e4672b635325aaab5f
Ali <alikiyand@gmail.com>
2018-05-06 21:46:52 +0100

    debug table structrure

--- src/jobWizardProject/LocationTable.php
+++ src/jobWizardProject/LocationTable.php
@@ -3,7 +3,7 @@
 namespace jobWizardProject;
 session_start();
 require_once __DIR__ . '/Location.php';
-require_once __DIR__ . '/TableAbstract.php';
+require_once __DIR__ . '/tableAbstract.php';
 require_once __DIR__ . '../../../Views/Template/includedFunctions.php';
 
 class LocationTable extends tableAbstract {
