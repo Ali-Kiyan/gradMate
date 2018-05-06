765bba8266384ed88aa35e9b0aca20a4b68ee0a8
Ali <alikiyand@gmail.com>
2018-05-06 13:00:05 +0100

    test

--- login.php
+++ login.php
@@ -7,16 +7,17 @@ require_once './vendor/autoload.php';
 
 if(isset($_POST['Lsubmit']))
 {
-    $database = new jobWizardProject\UserTable();
-    $result = $database->auth($_POST["Username"], $_POST["Password"]);
-    if($result)
-    {
-      (($_SESSION["Is_Admin"])==1?redirectTo("./adminDashboard.php"):redirectTo("./Dashboard.php"));
-    }
-    else
-    {
-        $view->result = '<div class="alert alert-danger">Username/Password is wrong </div>';
-    }
+    // $database = new jobWizardProject\UserTable();
+    // $result = $database->auth($_POST["Username"], $_POST["Password"]);
+    // if($result)
+    // {
+    //   (($_SESSION["Is_Admin"])==1?redirectTo("./adminDashboard.php"):redirectTo("./Dashboard.php"));
+    // }
+    // else
+    // {
+    //     $view->result = '<div class="alert alert-danger">Username/Password is wrong </div>';
+    // }
+    require_once './Views/login.phtml';
 
 }
 
