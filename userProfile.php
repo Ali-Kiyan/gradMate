95f1723fd687e75bca4303726ea48d3162dfc2be
Ali <alikiyand@gmail.com>
2018-05-06 22:39:14 +0100

    test

--- userProfile.php
+++ userProfile.php
@@ -5,7 +5,7 @@ $view = new stdClass();
 $view->pageTitle = 'User Profile';
 require_once  './vendor/autoload.php';
 $userdb = new JobWizardProject\UserTable();
-$Current_User = $userdb->fetchUserInfo($_SESSION['User_id']);
+// $Current_User = $userdb->fetchUserInfo($_SESSION['User_id']);
 // if(isset($_POST['Usubmit']))
 // {
 //     $userdb = new jobWizardProject\UserTable();
