142b5f7aeda8e9c42a113f14d044bb12f2e9d17f
Ali <alikiyand@gmail.com>
2018-05-07 11:17:56 +0100

    debuging new comapnies autoloader reequiring problem

--- addNewCompany.php
+++ addNewCompany.php
@@ -1,10 +1,10 @@
 <?php
-require('./Views/Template/includedFunctions.php');
+require_once('./Views/Template/includedFunctions.php');
 confirmAdmin();
 $view = new stdClass();
 $view->pageTitle = 'Add New Company';
 require_once  './vendor/autoload.php';
-
+//
 if(isset($_POST['Asubmit'])){
   $API = new jobWizardProject\ApiCompany();
   $generalInfo = $API->fetchGeneralInfo($_POST['Company_Name']);
@@ -12,31 +12,27 @@ if(isset($_POST['Asubmit'])){
 }
 
 
-$locationHandle = new jobWizardProject\LocationTable();
-$locations = $locationHandle->fetchLocations(0,1000000);
+      // $locationHandle = new jobWizardProject\LocationTable();
+      // $locations = $locationHandle->fetchLocations(0,1000000);
 
-if(isset($_POST['Isubmit']))
-{
-
-
-$_POST['Location_Id'] = settype($_POST['Location_Id'], int);
-    $companydb = new jobWizardProject\CompanyTable();
-    $respond = $companydb->insertCompany($_POST);
-    if($respond)
-    {
-
-        redirectTo("./newlyAddedCompanies.php");
-    }
-    else
-    {
-        $API = new jobWizardProject\ApiCompany();
-        $generalInfo = $API->fetchGeneralInfo($_POST['Company_Name']);
-        $Current_Company = $API->fetchCompanyInfo($generalInfo);
-        $view->result = '<div class="alert alert-danger">Please check your input </div>';
-    }
-
-
-}
+      // if(isset($_POST['Isubmit']))
+      // {
+      // $_POST['Location_Id'] = settype($_POST['Location_Id'], int);
+      //     $companydb = new jobWizardProject\CompanyTable();
+      //     $respond = $companydb->insertCompany($_POST);
+      //     if($respond)
+      //     {
+      //
+      //         redirectTo("./newlyAddedCompanies.php");
+      //     }
+      //     else
+      //     {
+      //         $API = new jobWizardProject\ApiCompany();
+      //         $generalInfo = $API->fetchGeneralInfo($_POST['Company_Name']);
+      //         $Current_Company = $API->fetchCompanyInfo($generalInfo);
+      //         $view->result = '<div class="alert alert-danger">Please check your input </div>';
+      //     }
+      //   }
 
 
 
--- src/jobWizardProject/ApiCompany.php
+++ src/jobWizardProject/ApiCompany.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace jobWizardProject;
-// session_start();
-// require_once __DIR__ . '../../../Views/Template/includedFunctions.php';
-require_once '../vendor/autoload.php';
+require_once __DIR__ . '../../../Views/Template/includedFunctions.php';
+
+require_once './vendor/autoload.php';
 require_once __DIR__ . '/Company.php';
 
 
