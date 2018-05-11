e1477e8f25895523e0fa285568d726fc22272ad8
Ali <alikiyand@gmail.com>
2018-05-11 16:04:46 +0100

    refactoring data set - final version

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,5 @@
-$(document).ready(function(){
-
+(function () {
   TweenLite.fromTo($('#usersTable'),2,{y: 30}, {y:0, opacity: 1});
-
-
-
-
 
   $('#showhideACC').click(function(){
     $('#ACC').slideToggle(400);
@@ -13,7 +8,7 @@ $(document).ready(function(){
   $('#showhideACC2').click(function(){
     $('#ACC2').slideToggle(400);
   });
-  
+
   $('#showhideACC3').click(function(){
     $('#ACC3').slideToggle(400);
   });
@@ -30,4 +25,7 @@ $(document).ready(function(){
       $(this).toggleClass("is-active",300);
     });
       TweenLite.fromTo($('#large-header'),1.5,{y:10,ease: "easeOut.config(1.7)"}, {y:0,opacity:1,ease: "easeOut.config(1.7)"});
-});
+      TweenLite.fromTo($('#login'),1.5,{x:50,y:100,rotation: "+=10",  rotationX: -50 }, {x:0,y:0,rotation: "-=10", rotationX:0,opacity: 1});
+      TweenLite.fromTo($('.signUpForm'),2.7,{z: -1150, rotationX: "+=120",rotationY: "+=10",ease: "Power4.easeOut"}, {y:150,opacity:1,rotationX: "0",rotationY: "0",ease: "Power4.easeOut"});
+
+})();
